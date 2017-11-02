74c1d2c35db5429a72eb243766be9bf8aa7d3126
FelixSeol <felixxseol@gmail.com>
2017-11-02 21:19:05 +0900

    almost done

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,21 @@
-function addValue();
-
-window.onload = function addValue(){
+function calculate(val){
 	var show = document.getElementById("show");
-	show.innerText = "3";
+	var str = show.innerHTML;
+	str += val;
+	show.innerHTML = str;
 }
 
-addValue()
+function myClear(){
+	var show = document.getElementById("show");
+	show.innerHTML = "";
+}
+
+function equal(){
+	var show = document.getElementById("show");
+	var str = show.innerHTML;
+	try{
+		show.innerHTML = eval(str);
+	}catch(e){
+		alert("Wrong Syntax.");
+	}
+}
